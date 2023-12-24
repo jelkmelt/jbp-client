fe028ecfecf28d176cc51de287805157bcd865c5
anonymous <anonymous@email.com>
2023-12-24 22:40:21 +0600

    removed required field from edit post number and email

--- pages/dashboard/edit-post/[postId].js
+++ pages/dashboard/edit-post/[postId].js
@@ -188,7 +188,7 @@ function EditPost() {
             value={state.email}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="name@flowbite.com"
-            required
+            // required
             onChange={handleChange}
           />
         </div>
@@ -208,7 +208,7 @@ function EditPost() {
             onWheel={onWheel2}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Contact number"
-            required
+            // required
             onChange={handleChange}
           />
         </div>
